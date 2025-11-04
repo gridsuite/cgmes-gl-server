d4c4802574142cb5d08441d1ef06a9256c1e7f9e
Abdelsalem <abdelsalem.hedhili@rte-france.com>
2025-11-04 10:35:16 +0100

    unused import
    Signed-off-by: Abdelsalem <abdelsalem.hedhili@rte-france.com>

--- src/test/java/org/gridsuite/cgmes/gl/server/services/CgmesGlServiceTest.java
+++ src/test/java/org/gridsuite/cgmes/gl/server/services/CgmesGlServiceTest.java
@@ -14,7 +14,6 @@ import org.gridsuite.cgmes.gl.server.utils.TestUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
