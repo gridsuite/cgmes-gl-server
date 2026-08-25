12164a6e532d9540cb0372f2e672b3204b1e310c
LE SAULNIER Kevin <kevin.lesaulnier.pro@gmail.com>
2026-08-25 09:54:28 +0200

    migrate rest template to rest client
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier.pro@gmail.com>

--- src/test/java/org/gridsuite/cgmes/gl/server/services/CgmesGlServiceTest.java
+++ src/test/java/org/gridsuite/cgmes/gl/server/services/CgmesGlServiceTest.java
@@ -11,14 +11,19 @@ import com.powsybl.cgmes.conformity.CgmesConformity1Catalog;
 import com.powsybl.cgmes.model.GridModelReferenceResources;
 import com.powsybl.iidm.network.Network;
 import org.gridsuite.cgmes.gl.server.utils.TestUtils;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
+import org.springframework.test.web.client.MockRestServiceServer;
 
 import java.util.HashSet;
 import java.util.UUID;
@@ -28,33 +33,32 @@ import static org.mockito.AdditionalAnswers.delegatesTo;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 /**
  * @author Chamseddine Benhamed <chamseddine.benhamed at rte-france.com>
  */
-@ExtendWith(MockitoExtension.class)
+@RestClientTest(CgmesGlService.class)
 class CgmesGlServiceTest {
 
-    @Mock
-    private RestTemplate geoDataServerRest; //halt http client
+    @Autowired
+    private MockRestServiceServer geoDataServer;
 
     @Mock
     private CaseDataSourceClient caseServerDataSource; //halt database
 
-    @Mock
-    private RestTemplateBuilder restTemplateBuilder;
-
+    @MockitoSpyBean
     private CgmesGlService cgmesGlService;
 
     private static final UUID CASE_UUID = UUID.randomUUID();
 
     @BeforeEach
-    void mockCaseServer() {
-        when(restTemplateBuilder.rootUri(Mockito.any())).thenReturn(restTemplateBuilder);
-        when(restTemplateBuilder.build()).thenReturn(geoDataServerRest);
-        cgmesGlService = Mockito.spy(new CgmesGlService("https://localhost:8087", "https://localhost:8085", restTemplateBuilder));
-
-        GridModelReferenceResources gridModel = CgmesConformity1Catalog.microGridBaseCaseBE();
+    void setUp() {
+        GridModelReferenceResources gridModel =
+            CgmesConformity1Catalog.microGridBaseCaseBE();
 
         when(caseServerDataSource.newInputStream(anyString())).then(delegatesTo(gridModel.dataSource()));
         when(caseServerDataSource.getBaseName()).then(delegatesTo(gridModel.dataSource()));
@@ -64,6 +68,11 @@ class CgmesGlServiceTest {
         doReturn(caseServerDataSource).when(cgmesGlService).createCaseServerDataSource(CASE_UUID);
     }
 
+    @AfterEach
+    void tearDown() {
+        geoDataServer.verify();
+    }
+
     @Test
     void test() {
         Network network = cgmesGlService.getNetwork(CASE_UUID);
@@ -71,7 +80,24 @@ class CgmesGlServiceTest {
 
         TestUtils.checkExtensions(network, new HashSet<>());
 
+        geoDataServer.expect(requestTo(
+                "http://localhost:8087/" +
+                    CgmesGlConstants.GEO_DATA_API_VERSION +
+                    "/substations"
+            ))
+            .andExpect(method(HttpMethod.POST))
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andRespond(withSuccess());
+
+        geoDataServer.expect(requestTo(
+                "http://localhost:8087/" +
+                    CgmesGlConstants.GEO_DATA_API_VERSION +
+                    "/lines"
+            ))
+            .andExpect(method(HttpMethod.POST))
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andRespond(withSuccess());
+
         cgmesGlService.toGeoDataServer(CASE_UUID, new HashSet<>());
-        //TODO missing assertion(s) of result
     }
 }
